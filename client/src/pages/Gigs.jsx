3a4d4e7348428593452b1e48e6249fe6251c02f2
mleftwich <mleftwich@live.com>
2022-10-11 00:45:15 +0800

    Landing page
    Landing page and navigation fleshed out

---
+++ client/src/pages/Gigs.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+import { Box } from "@mui/system";
+
+const styles = {
+  container: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  heading: {
+    color: "white",
+    fontFamily: "Bungee Shade, cursive",
+    textAlign: "center",
+    textShadow: "2px 2px 4px black",
+    fontSize: "2.5rem",
+  },
+  background: {
+    backgroundColor: "rgb(0, 0, 0, 0.6)",
+    color: "white",
+    border: "1px solid rgba(255, 255, 255, 0.2)",
+    paddingLeft: "5px",
+    paddingRight: "5px",
+    borderRadius: 5,
+  },
+};
+export function Gigs() {
+  return (
+    <div>
+      <div style={styles.container}>
+        <Box
+          sx={{
+            width: { xs: 400, sm: 500, md: 700, lg: 900, xl: 1000 },
+            backgroundColor: "rgb(0, 0, 0, 0.7)",
+            borderRadius: 3,
+            transition: "ease in",
+            margin: 1,
+            marginTop: 3,
+
+            padding: 1.5,
+          }}
+        >
+          <div style={styles.background}>
+            <p>dasdasfaadf</p>
+          </div>
+        </Box>
+      </div>
+    </div>
+  );
+}
